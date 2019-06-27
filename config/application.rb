b6d3fd50caff7b6a860dbe89f16e6bbf5392343d
Keisuke Taniguchi <rememberthatk@icloud.com>
2019-06-27 18:58:52 +0900

    [master] setup config

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,17 @@ Bundler.require(*Rails.groups)
 
 module RailsSample1
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.decorator false
+      g.test_framework nil
+    end
   end
 end
