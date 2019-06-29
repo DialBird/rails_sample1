6964a0b87749c9142ab3f28d6ad56630e3dbde29
Keisuke Taniguchi <rememberthatk@icloud.com>
2019-06-29 11:50:26 +0900

    [master] add auth0

---
+++ config/initializers/auth0.rb
@@ -0,0 +1,12 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider(
+    :auth0,
+    'CYVj8R33M6O4D2cnd1ueDB6oCC1Id1yP',
+    Rails.application.credentials.auth0[:secret],
+    'dialbird.au.auth0.com',
+    callback_path: '/auth/oauth2/callback',
+    authorize_params: {
+      scope:  'openid profile'
+    }
+  )
+end
