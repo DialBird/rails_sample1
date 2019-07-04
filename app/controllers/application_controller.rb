3651ca8b27ea1c4b152be5213017074a52ca8da6
Keisuke Taniguchi <rememberthatk@icloud.com>
2019-07-04 18:41:57 +0900

    [master] add routing error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :user_signed_in?
 
+  unless Rails.env.development?
+    rescue_from Exception,                      with: :render_500
+    rescue_from ActiveRecord::RecordNotFound,   with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+  end
+
   def authenticate_user!
     return if user_signed_in?
 
@@ -25,5 +31,26 @@ class ApplicationController < ActionController::Base
     return unless session[:userinfo]
 
     User.find_by(id: session[:userinfo][0], email: session[:userinfo][1])
+  end
+
+  def render_404
+    if request.format.to_sym == :json
+      render json: { error: '404 error' }, status: :not_found
+    else
+      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+    end
+  end
+
+  def render_500(err = nil)
+    logger.debug(<<~LOG)
+      500 Error message starts
+      #{err.class} (#{err.message}):
+      #{Rails.backtrace_cleaner.clean(err.backtrace).join("\n").indent(1)}
+    LOG
+    if request.format.to_sym == :json
+      render json: { error: '500 error' }, status: :internal_server_error
+    else
+      render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
+    end
   end
 end
